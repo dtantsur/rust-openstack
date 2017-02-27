582c46ed340694d7ec831aa936e947877172da87
Dmitry Tantsur <divius.inside@gmail.com>
2017-02-27 15:22:18 +0100

    Apparently I don't know what year it is now

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-// Copyright 2016 Dmitry Tantsur <divius.inside@gmail.com>
+// Copyright 2017 Dmitry Tantsur <divius.inside@gmail.com>
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
