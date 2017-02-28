a122e1549467359d99dd9d8fd2be44a29aff02de
Dmitry Tantsur <divius.inside@gmail.com>
2017-02-28 19:08:40 +0100

    Add missing unit tests for catalog::find_endpoint

--- src/identity/catalog.rs
+++ src/identity/catalog.rs
@@ -57,3 +57,121 @@ pub fn find_endpoint<'a>(catalog: &'a Catalog, service_type: &str,
         None => Err(ApiError::EndpointNotFound)
     }
 }
+
+
+#[cfg(test)]
+pub mod test {
+    use super::super::super::ApiError;
+    use super::super::protocol::{CatalogRecord, Endpoint};
+    use super::{Catalog, find_endpoint};
+
+    fn demo_service1() -> CatalogRecord {
+        CatalogRecord {
+            id: String::from("1"),
+            service_type: String::from("identity"),
+            name: String::from("keystone"),
+            endpoints: vec![
+                Endpoint {
+                    id: String::from("e1"),
+                    interface: String::from("public"),
+                    region: String::from("RegionOne"),
+                    url: String::from("https://host.one/identity")
+                },
+                Endpoint {
+                    id: String::from("e2"),
+                    interface: String::from("internal"),
+                    region: String::from("RegionOne"),
+                    url: String::from("http://192.168.22.1/identity")
+                },
+                Endpoint {
+                    id: String::from("e3"),
+                    interface: String::from("public"),
+                    region: String::from("RegionTwo"),
+                    url: String::from("https://host.two:5000")
+                }
+            ]
+        }
+    }
+
+    fn demo_service2() -> CatalogRecord {
+        CatalogRecord {
+            id: String::from("2"),
+            service_type: String::from("baremetal"),
+            name: String::from("ironic"),
+            endpoints: vec![
+                Endpoint {
+                    id: String::from("e4"),
+                    interface: String::from("public"),
+                    region: String::from("RegionOne"),
+                    url: String::from("https://host.one/baremetal")
+                },
+                Endpoint {
+                    id: String::from("e5"),
+                    interface: String::from("public"),
+                    region: String::from("RegionTwo"),
+                    url: String::from("https://host.two:6385")
+                }
+            ]
+        }
+    }
+
+    pub fn demo_catalog() -> Catalog {
+        vec![demo_service1(), demo_service2()]
+    }
+
+    #[test]
+    fn test_find_endpoint() {
+        let cat = demo_catalog();
+
+        let e1 = find_endpoint(&cat, "identity", "public", None).unwrap();
+        assert_eq!(&e1.id, "e1");
+        assert_eq!(&e1.url, "https://host.one/identity");
+
+        let e2 = find_endpoint(&cat, "identity", "internal", None).unwrap();
+        assert_eq!(&e2.id, "e2");
+        assert_eq!(&e2.url, "http://192.168.22.1/identity");
+
+        let e3 = find_endpoint(&cat, "baremetal", "public", None).unwrap();
+        assert_eq!(&e3.id, "e4");
+        assert_eq!(&e3.url, "https://host.one/baremetal");
+    }
+
+    #[test]
+    fn test_find_endpoint_with_region() {
+        let cat = demo_catalog();
+
+        let e1 = find_endpoint(&cat, "identity", "public",
+                               Some("RegionTwo")).unwrap();
+        assert_eq!(&e1.id, "e3");
+        assert_eq!(&e1.url, "https://host.two:5000");
+
+        let e2 = find_endpoint(&cat, "identity", "internal",
+                               Some("RegionOne")).unwrap();
+        assert_eq!(&e2.id, "e2");
+        assert_eq!(&e2.url, "http://192.168.22.1/identity");
+
+        let e3 = find_endpoint(&cat, "baremetal", "public",
+                               Some("RegionTwo")).unwrap();
+        assert_eq!(&e3.id, "e5");
+        assert_eq!(&e3.url, "https://host.two:6385");
+    }
+
+    fn assert_not_found(result: Result<&Endpoint, ApiError>) {
+        match result.err().unwrap() {
+            ApiError::EndpointNotFound => (),
+            other => panic!("Unexpected error {}", other)
+        }
+    }
+
+    #[test]
+    fn test_find_endpoint_not_found() {
+        let cat = demo_catalog();
+
+        assert_not_found(find_endpoint(&cat, "foobar", "public", None));
+        assert_not_found(find_endpoint(&cat, "identity", "public",
+                                       Some("RegionFoo")));
+        assert_not_found(find_endpoint(&cat, "baremetal", "internal", None));
+        assert_not_found(find_endpoint(&cat, "identity", "internal",
+                                       Some("RegionTwo")));
+    }
+}
