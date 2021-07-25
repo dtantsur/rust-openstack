887895ca770d71632db4f2812c2be45e4aeb4d3d
Julien Fastré <julienfastre@users.noreply.github.com>
2021-07-25 10:51:19 +0200

    feat(object-storage): add hash to objects (#125)
    * add examples for object storage
    
    * [object-store] add hash to objects
    
    * replace hash type, using Option<String> instead of String

--- src/object_storage/objects.rs
+++ src/object_storage/objects.rs
@@ -139,6 +139,11 @@ impl Object {
         #[doc = "Object content type (if set)."]
         content_type: ref Option<String>
     }
+    
+    transparent_property! {
+        #[doc = "Object hash or ETag, which is a content's md5 hash"]
+        hash: ref Option<String>
+    }
 
     transparent_property! {
         #[doc = "Object name."]
