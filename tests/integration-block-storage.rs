9aeece22a0cebebab23f94b66b17b91e811703ab
Sandro-Alessio Gierens <sandro@gierens.de>
2024-03-17 08:56:56 +0100

    test(block_storage): fix test_volume_create_get_delete_simple, name is empty
    Even though its renamed with the UUID later on, the volume returned on
    creation has an empty string as name.
    
    Signed-off-by: Sandro-Alessio Gierens <sandro@gierens.de>

--- tests/integration-block-storage.rs
+++ tests/integration-block-storage.rs
@@ -36,7 +36,7 @@ async fn test_volume_create_get_delete_simple() {
         .await
         .expect("Could not create volume");
     let id = volume.id().clone();
-    assert_eq!(*volume.name(), id);
+    assert!(volume.name().is_empty());
     assert!(volume.description().is_none());
     assert_eq!(*volume.size(), 1 as u64);
 
